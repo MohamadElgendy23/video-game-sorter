ef6c05016abb8105c0a3c01a150c727ef7b21ee3
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-03-28 14:26:47 -0600

    currently creating the reviewservice.

--- src/main/java/com/example/video_game_sorter/Controller/ReviewController.java
+++ src/main/java/com/example/video_game_sorter/Controller/ReviewController.java
@@ -1,9 +1,23 @@
 package com.example.video_game_sorter.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.video_game_sorter.Models.VideoGame;
+import com.example.video_game_sorter.Service.ReviewService;
+
 // This class represents the review controller; ie. the routes for a review
 @RestController
 public class ReviewController {
     
     @Autowired
-    private VideoGameService videoGameService;
+    private ReviewService reviewService;
+
+    @GetMapping(value="/reviews")
+    public List<VideoGame> getReviews() {
+        return reviewService.getReviews();
+    }
 }
--- src/main/java/com/example/video_game_sorter/Service/ReviewService.java
+++ src/main/java/com/example/video_game_sorter/Service/ReviewService.java
@@ -1,8 +1,11 @@
 package com.example.video_game_sorter.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.video_game_sorter.Models.Review;
 import com.example.video_game_sorter.Repo.ReviewRepo;
 
 @Service
@@ -11,5 +14,11 @@ public class ReviewService {
     @Autowired
     private ReviewRepo reviewRepository;
 
-    
+    public List<Review> getReviews() {
+        return reviewRepository.findAll();
+    }
+
+    public void crea(VideoGame videoGame) {
+        videoGameRepository.save(videoGame);
+    }
 }
