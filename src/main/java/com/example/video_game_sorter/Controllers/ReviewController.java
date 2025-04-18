c8b6d11fd24cacf1430033fb49a6fee8f9b31a1a
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-04-17 23:02:02 -0600

    added cors into the springboot backend so the frontend can call the apis

--- src/main/java/com/example/video_game_sorter/Controllers/ReviewController.java
+++ src/main/java/com/example/video_game_sorter/Controllers/ReviewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 // This class represents the review controller; ie. the routes for a review
 @RestController
 @RequestMapping(value="/api/reviews")
+@CrossOrigin(origins="http://localhost:3000")
 public class ReviewController {
     
     @Autowired
--- src/main/java/com/example/video_game_sorter/Controllers/VideoGameController.java
+++ src/main/java/com/example/video_game_sorter/Controllers/VideoGameController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 // This class represents the video game controller; ie. the routes for a video game
 @RestController
 @RequestMapping("/api/videogames")
+@CrossOrigin(origins="http://localhost:3000")
 public class VideoGameController {
     
     @Autowired
